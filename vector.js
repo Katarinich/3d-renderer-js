c9fbb9cc95aa31fc8002f2414669c2fd32e625e5
Katarinich <katarina.chirich@gmail.com>
2020-06-04 11:32:53 +0300

    Added vector class

---
+++ vector.js
@@ -0,0 +1,23 @@
+export class Vector {
+  constructor(x, y, z) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+  }
+
+  add(vector) {
+    const { x, y, z } = vector;
+
+    return new Vector(this.x + x, this.y + y, this.z + z);
+  }
+
+  subtract(vector) {
+    const { x, y, z } = vector;
+
+    return new Vector(this.x - x, this.y - y, this.z - z);
+  }
+
+  scale(number) {
+    return new Vector(this.x * number, this.y * number, this.z * number);
+  }
+}
