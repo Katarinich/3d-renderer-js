38bf781f5807ae99b7586b50b383dcb0d0420375
Katarinich <katarina.chirich@gmail.com>
2020-06-04 19:37:39 +0300

    Added dot product method to vector

--- vector.js
+++ vector.js
@@ -21,6 +21,12 @@ class Vector {
     return new Vector(this.x * number, this.y * number, this.z * number);
   }
 
+  dotProduct(vector) {
+    const { x, y, z } = vector;
+
+    return this.x * x + this.y * y + this.z * z;
+  }
+
   static linearInterpolation(startVector, endVector, alpha) {
     return startVector.scale(1 - alpha).add(endVector.scale(alpha));
   }
